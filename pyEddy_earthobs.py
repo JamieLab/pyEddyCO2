982d695fc2d9a6969022869621f92f8de6724604
Daniel Ford <77839711+danielford166@users.noreply.github.com>
2024-01-02 06:59:56 +0000

    Updates to get code working on extracted data

--- pyEddy_earthobs.py
+++ pyEddy_earthobs.py
@@ -8,7 +8,7 @@ import glob
 import matplotlib.path as mpltPath
 import matplotlib.pyplot as plt
 import pyEddy_main as pyEddy_m
-import morel91 as pp
+import calendar
 
 def timeseries_start(eddy, track_no):
     f = np.squeeze(np.argwhere(eddy['track'] == track_no))
@@ -87,9 +87,16 @@ def load_file(set,latv,lonv,latc,lonc,radmax,radm,lat=[],lon=[]):
     if (len(lat) == 0) & (len(lon) == 0):
         lat = np.squeeze(set.variables[latv])
         lon = np.squeeze(set.variables[lonv])
-    if np.max(lon) > 180:
-        f = np.argwhere(lon > 180)
-        lon = lon - 360
+
+    # if np.max(lon) > 180:
+    #     f = np.argwhere(lon > 180)
+    #     lon = lon - 360
+    lon_len = len(lon)
+    lat,lon,f,g = find_f_g(lat,lon,latc,lonc,radmax,radm)
+
+    return lat,lon,f,g,lon_len
+
+def find_f_g(lat,lon,latc,lonc,radmax,radm):
     f = np.squeeze(np.argwhere((lat <= latc + radmax*radm) & (lat >= latc - radmax*radm)))
     g = np.squeeze(np.argwhere((lon <= lonc + radmax*radm) & (lon >= lonc - radmax*radm)))
     #var = np.squeeze(set.variables[datav][0,f,g])
@@ -150,12 +157,28 @@ def add_sstCCI(loc,eddy,desc,radm = 3,plot=0):
             # else:
             #     radmax = rad[1]
             c = Dataset(file[0])
-            lat,lon,f,g = load_file(c,'lat','lon',latc,lonc,radmax,radm)
-            sst = np.squeeze(c.variables['analysed_sst'][0,f,g])
-            sst[sst.mask==True] = np.nan
-            sst = sst.data
-            lat,lon,f,g = load_file(c,'lat','lon',latc,lonc,radmax,radm)
-            sst_u = np.squeeze(c.variables['analysed_sst_uncertainty'][0,f,g])
+            lon =c.variables['lon'][:]; lat = c.variables['lat'][:]
+            #lat,lon,f,g = load_file(c,'lat','lon',latc,lonc,radmax,radm)
+            if (lonc - (radmax*radm) < lon[0]) or (lonc + (radmax*radm) > lon[-1]):
+                print('First')
+                sst = np.squeeze(c.variables['analysed_sst'][0,:,:])
+                sst[sst.mask==True] = np.nan
+                sst_u = np.squeeze(c.variables['analysed_sst_uncertainty'][0,:,:])
+                sst_u[sst_u.mask==True] = np.nan
+                lon2 = np.copy(lon)
+                #print(sst.shape)
+                #lon =c.variables['longitude'][:]; lat = c.variables['latitude'][:]
+                lon,sst = grid_switch(lon,sst)
+                lon2,sst_u = grid_switch(lon,sst_u)
+                lat,lon,f,g = find_f_g(lat,lon,latc,lonc,radmax,radm)
+                sst=sst[np.ix_(f,g)]
+                sst_u=sst_u[np.ix_(f,g)]
+            else:
+                print('Second')
+                lat,lon,f,g = find_f_g(lat,lon,latc,lonc,radmax,radm)
+                sst = np.squeeze(c.variables['analysed_sst'][0,f,g])
+                sst_u = np.squeeze(c.variables['analysed_sst_uncertainty'][0,f,g])
+
             c.close()
 
             if plot == 1:
@@ -173,10 +196,8 @@ def add_sstCCI(loc,eddy,desc,radm = 3,plot=0):
     eddy,desc = inoutsplit(eddy,sstout,'cci_sst_out',desc,'kelvin')
     return eddy,desc
 
-def add_MODchl(loc,eddy,desc,radm = 3,plot=0):
-    # Add chl-a values from MODIS-A downloaded from NASA OceanColour data website
-    #print(os.path.join(loc,date.strftime("%Y"),str(julian(date)),date.strftime("A%Y*.nc")))
-
+def add_cmems(loc,eddy,desc,var='so',units='psu',radm = 3,plot=0,log10=False,depth=True):
+    # Function to extract sstCCI data for the eddy and the surrounding environment.
     sstin = np.empty((len(eddy['time']),6))
     sstin[:] = np.nan
     sstout = np.empty((len(eddy['time']),6))
@@ -189,16 +210,39 @@ def add_MODchl(loc,eddy,desc,radm = 3,plot=0):
         lons = eddy['effective_contour_longitude'][t,:]
         lats = eddy['effective_contour_latitude'][t,:]
         if (lats[0] != 0.0) & (lons[0] != 180.0):
-            file = glob.glob(os.path.join(loc,date.strftime("%Y"),str(julian(date)),date.strftime("A%Y*.nc")))
+            file = glob.glob(os.path.join(loc,date.strftime("%Y"),date.strftime("%Y_%m*.nc")))
+            day_val = date.day-1 # -1 as index starts at 0
+            print(day_val)
             # if radius_check(lonc,latc,lons,lats) > rad[1]:
             radmax = radius_check(lonc,latc,lons,lats)
             # else:
             #     radmax = rad[1]
             c = Dataset(file[0])
-            lat,lon,f,g = load_file(c,'lat','lon',latc,lonc,radmax,radm)
-            sst = np.log10(np.squeeze(c.variables['chlor_a'][f,g]))
-            sst[sst.mask==True] = np.nan
-            sst = sst.data
+            lon =c.variables['longitude'][:]; lat = c.variables['latitude'][:]
+            #lat,lon,f,g = load_file(c,'lat','lon',latc,lonc,radmax,radm)
+            if (lonc - (radmax*radm) < lon[0]) or (lonc + (radmax*radm) > lon[-1]):
+                print('First')
+                if depth:
+                    sst = np.squeeze(c.variables[var][day_val,0,:,:])
+                else:
+                    sst = np.squeeze(c.variables[var][day_val,:,:])
+                sst[sst.mask==True] = np.nan
+                if log10:
+                    sst = np.log10(sst)
+
+                lon,sst = grid_switch(lon,sst)
+
+                lat,lon,f,g = find_f_g(lat,lon,latc,lonc,radmax,radm)
+                sst=sst[np.ix_(f,g)]
+            else:
+                print('Second')
+                lat,lon,f,g = find_f_g(lat,lon,latc,lonc,radmax,radm)
+                if depth:
+                    sst = np.squeeze(c.variables[var][day_val,0,f,g])
+                else:
+                    sst = np.squeeze(c.variables[var][day_val,f,g])
+                if log10:
+                    sst = np.log10(sst)
             c.close()
 
             if plot == 1:
@@ -212,10 +256,10 @@ def add_MODchl(loc,eddy,desc,radm = 3,plot=0):
             #lons,lats = ellipse(lonc,latc,rad[0],rad[1])
             inp = pointsineddy(lon,lat,lons,lats)
             sstin[t,:],sstout[t,:] = inouteddy(sst,inp)
-    #print(sstout)
-    eddy,desc = inoutsplit(eddy,sstin,'modis_chla_in',desc,'log10(mg/m3)')
-    eddy,desc = inoutsplit(eddy,sstout,'modis_chla_out',desc,'log10(mg/m3)')
+    eddy,desc = inoutsplit(eddy,sstin,f'cmems_{var}_in',desc,units)
+    eddy,desc = inoutsplit(eddy,sstout,f'cmems_{var}_out',desc,units)
     return eddy,desc
+
 
 def add_wind(loc,eddy,desc,radm = 3,plot=0):
     #
@@ -227,24 +271,50 @@ def add_wind(loc,eddy,desc,radm = 3,plot=0):
         print(t)
         date = pyEddy_m.date_con(eddy['time'][t])
         lonc = eddy['longitude'][t]
+        # if lonc<0:
+        #     lonc = lonc+360
         latc = eddy['latitude'][t]
         lons = eddy['effective_contour_longitude'][t,:]
+        #lons[lons<0] = lons[lons<0] + 360
         lats = eddy['effective_contour_latitude'][t,:]
         if (lats[0] != 0.0) & (lons[0] != 180.0):
+            print(os.path.join(loc,date.strftime("Y%Y"),date.strftime("M%m"),date.strftime("CCMP_Wind_Analysis_%Y%m%d*.nc")))
             file = glob.glob(os.path.join(loc,date.strftime("Y%Y"),date.strftime("M%m"),date.strftime("CCMP_Wind_Analysis_%Y%m%d*.nc")))
             # if radius_check(lonc,latc,lons,lats) > rad[1]:
             radmax = radius_check(lonc,latc,lons,lats)
             # else:
             #     radmax = rad[1]
             c = Dataset(file[0])
-            lat,lon,f,g = load_file(c,'latitude','longitude',latc,lonc,radmax,radm)
-            uwnd = np.squeeze(c.variables['uwnd'][:,f,g])
-            print(uwnd.shape)
-            vwnd = np.squeeze(c.variables['vwnd'][:,f,g])
-            sst = np.squeeze(np.nanmean(np.sqrt(uwnd**2 + vwnd**2),axis=0))
-            sst[sst.mask==True] = np.nan
-            sst = sst.data
+            lon =c.variables['longitude'][:]; lat = c.variables['latitude'][:]
+            #lat,lon,f,g,lon_len = load_file(c,'latitude','longitude',latc,lonc,radmax,radm)
+
+            # print(lon_len)
+            # print(len(g))
+            print(lon[0])
+            print(lonc - (radmax*radm))
+            print(lon[-1])
+            print(lonc + (radmax*radm))
+            if (lonc - (radmax*radm) < lon[0]) or (lonc + (radmax*radm) > lon[-1]):
+                print('First')
+                sst = np.squeeze(np.nanmean(c.variables['ws'][:,:,:],axis=2))
+                #print(sst.shape)
+                #lon =c.variables['longitude'][:]; lat = c.variables['latitude'][:]
+                lon,sst = grid_switch(lon,sst)
+                lat,lon,f,g = find_f_g(lat,lon,latc,lonc,radmax,radm)
+                # print(f)
+                # print(g)
+                sst=sst[np.ix_(f,g)]
+            else:
+                print('Second')
+                lat,lon,f,g = find_f_g(lat,lon,latc,lonc,radmax,radm)
+                sst = np.squeeze(np.nanmean(c.variables['ws'][f,g,:],axis=2))
+            #print(f)
+            #print(g)
             c.close()
+            print(sst.shape)
+            # sst[sst.mask==True] = np.nan
+            # sst = sst.data
+
 
             if plot == 1:
                 f,ax1 = plt.subplots()
@@ -256,50 +326,118 @@ def add_wind(loc,eddy,desc,radm = 3,plot=0):
                 #ax1.plot(cix,ciy,'r--')
                 ax1.plot(lons,lats,'b--')
                 plt.show()
-            #lons,lats = ellipse(lonc,latc,rad[0],rad[1])
+
             inp = pointsineddy(lon,lat,lons,lats)
             sstin[t,:],sstout[t,:] = inouteddy(sst,inp)
     eddy,desc = inoutsplit(eddy,sstin,'ccmp_wind_in',desc,'ms-1')
     eddy,desc = inoutsplit(eddy,sstout,'ccmp_wind_out',desc,'ms-1')
     return eddy,desc
 
-def add_GlobPAR(loc,eddy,desc,radm = 3,plot=0):
-    sstin = np.empty((len(eddy['time']),6))
-    sstin[:] = np.nan
-    sstout = np.empty((len(eddy['time']),6))
-    sstout[:] = np.nan
-    for t in range(0,len(eddy['time'])):
-        print(t)
-        date = pyEddy_m.date_con(eddy['time'][t])
-        lonc = eddy['longitude'][t]
-        latc = eddy['latitude'][t]
-        lons = eddy['effective_contour_longitude'][t,:]
-        lats = eddy['effective_contour_latitude'][t,:]
-        if (lats[0] != 0.0) & (lons[0] != 180.0):
-            file = glob.glob(os.path.join(loc,date.strftime("L3m_%Y%m%d*.nc")))
-            # if radius_check(lonc,latc,lons,lats) > rad[1]:
-            radmax = radius_check(lonc,latc,lons,lats)
-            # else:
-            #     radmax = rad[1]
-            c = Dataset(file[0])
-            lat,lon,f,g = load_file(c,'lat','lon',latc,lonc,radmax,radm)
-            sst = np.squeeze(c.variables['PAR_mean'][f,g])
-            sst[sst.mask==True] = np.nan
-            sst = sst.data
-            c.close()
+def grid_switch(lon,var):
+    """
+    Function to switch a global grid from 0-360E to -180 to 180E. This requires the
+    array to be manually modified.
+    """
 
-            if plot == 1:
-                f,ax1 = plt.subplots()
-                m = ax1.pcolor(lon,lat,sst)
-                plt.colorbar(m)
-                ax1.plot(lons,lats,'b--')
-                # cix,ciy = ellipse(lonc,latc,rad[0],rad[1])
-                # ax1.plot(cix,ciy,'r--')
-                plt.show()
-            #lons,lats = ellipse(lonc,latc,rad[0],rad[1])
-            inp = pointsineddy(lon,lat,lons,lats)
-            sstin[t,:],sstout[t,:] = inouteddy(sst,inp)
-    #print(sstout)
-    eddy,desc = inoutsplit(eddy,sstin,'glob_par_in',desc,'einstein/m2/day')
-    eddy,desc = inoutsplit(eddy,sstout,'glob_par_out',desc,'einstein/m2/day')
-    return eddy,desc
+    #print(var_sh)
+
+    if var.shape[0] == len(lon):
+        var_sh = int(var.shape[0]/2)
+        var_temp = np.empty((var.shape))
+        var_temp[0:var_sh,:] = var[var_sh:,:]
+        var_temp[var_sh:,:] = var[0:var_sh,:]
+    else:
+        var_sh = int(var.shape[1]/2)
+        var_temp = np.empty((var.shape))
+        var_temp[:,0:var_sh] = var[:,var_sh:]
+        var_temp[:,var_sh:] = var[:,0:var_sh]
+    #lon_temp = np.empty((var_sh*2))
+    lon = lon-180
+    # lon_temp[var_sh:] = lon[0:var_sh]
+    # lon_temp[0:var_sh] = lon[var_sh:]
+    return lon,var_temp
+
+def produce_monthly(track,s_loc,vars=[]):
+    vars.append('latitude')
+    vars.append('longitude')
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'r')
+    time = np.array(c['time'])
+
+    time_2 = np.zeros((len(time),2))
+    for i in range(len(time)):
+        time_2[i,0] = pyEddy_m.date_con(int(time[i])).month
+        time_2[i,1] = pyEddy_m.date_con(int(time[i])).year
+
+    max_year = np.max(time_2[:,1])
+    min_year = np.min(time_2[:,1])
+
+    time_m = []
+    out_time = []
+    ye = min_year
+    mon = 1
+    while ye < max_year+1:
+        f = np.where((time_2[:,0] == mon) & (time_2[:,1] == ye))[0]
+        month_r = calendar.monthrange(int(ye),int(mon))[1]
+        if len(f) == month_r:
+            time_m.append([f])
+            out_time.append(pyEddy_m.date_convert(datetime.datetime(int(ye),int(mon),15)))
+        mon=mon+1
+        if mon == 13:
+            ye=ye+1
+            mon=1
+    out = {}
+    for v in vars:
+        data = np.array(c[v])
+        data_o = np.zeros((len(out_time))); data_o[:] = np.nan
+        for i in range(len(out_time)):
+            data_o[i] = np.nanmean(data[time_m[i]])
+        out[v] = data_o
+    c.close()
+
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+    try:
+        c.createDimension('month_time',len(time_m))
+    except:
+        print('Dimension exists?')
+    if 'month_time' in c.variables.keys():
+        c['month_time'][:] = np.array(out_time)
+    else:
+        m = c.createVariable('month_time',np.float32,('month_time'))
+        m[:] = np.array(out_time)
+    c['month_time'].units = "days since 1950-01-01 00:00:00"
+    for v in vars:
+        if 'month_'+v in c.variables.keys():
+            c['month_'+v][:] = out[v]
+        else:
+            m = c.createVariable('month_'+v,np.float32,('month_time'))
+            m[:] = out[v]
+    c.close()
+
+def add_noaa(loc,s_loc,track):
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'r')
+    time = np.array(c['month_time'])
+    lat = np.array(c['month_latitude'])
+    lon = np.array(c['month_longitude'])
+    c.close()
+    xco2 = np.zeros((len(time))); xco2[:] = np.nan
+    for i in range(len(time)):
+        timed = pyEddy_m.date_con(int(time[i]))
+        file = glob.glob(os.path.join(loc,timed.strftime("%Y_%m_*.nc")))
+        c = Dataset(file[0],'r')
+        latg = np.abs(np.array(c['latitude']) - lat[i])
+        long = np.abs(np.array(c['longitude']) - lon[i])
+
+        f = np.where(latg == np.min(latg))[0]
+        #print(f)
+        g = np.where(long == np.min(long))[0]
+        #print(g)
+        xco2[i] = np.array(c.variables['xCO2'][g,f])
+        c.close()
+
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+    if 'month_xco2' in c.variables.keys():
+        c['month_xco2'][:] = np.array(xco2)
+    else:
+        m = c.createVariable('month_xco2',np.float32,('month_time'))
+        m[:] = np.array(xco2)
+    c['month_xco2'].units = 'uatm'
--- pyEddy_ex.py
+++ pyEddy_ex.py
@@ -8,42 +8,42 @@ import numpy as np
 from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 
+output_loc = 'OUTPUT'
 # To save speed and memory these are the variables in the netcdf we don't want to load. This can be modified,
 # depending on what is required
-no_load = ['cost_association','speed_contour_height','speed_contour_latitude','speed_contour_longitude','speed_contour_shape_error',
-    'speed_average','speed_area','inner_contour_height','latitude_max','longitude_max','num_point_s','uavg_profile']
-# File where our eddy data is located
-file = 'D:/Data/AVISO_EDDIES/META3.2_DT_allsat_Anticyclonic_long_19930101_20220209.nc'
-# Loading the eddy netcdf data into a dictionary that corresponds to the netcdf file variable names.
-# This will not load anything that is defined in "no_load". If you want to load all variables leave
-# no_load empty (i.e [])
-eddy,desc = pyEddy_m.AVISO_load(file,no_load)
-print(desc)
-# # Here we split the eddy dict down to a set spatial region, and temporal window. This will
-# # include all eddies even if they appear in the domain for 1 day. We can perform checks later that
-# # they remain in the domain for a period. If you want all the data, then you don't need to use
-# # this function.
-print(len(np.unique(eddy['track'])))
-
-eddy = pyEddy_m.box_split(eddy,[-60,10],[-70,25],[datetime.datetime(2002,7,1),datetime.datetime(2018,12,31)])
-print(len(np.unique(eddy['track'])))
-
-eddy = pyEddy_m.eddy_length_min(eddy,365)
-print(len(np.unique(eddy['track'])))
-
+# no_load = ['cost_association','speed_contour_height','speed_contour_latitude','speed_contour_longitude','speed_contour_shape_error',
+#     'speed_average','speed_area','inner_contour_height','latitude_max','longitude_max','num_point_s','uavg_profile']
+# # File where our eddy data is located
+# file = 'D:/Data/AVISO_EDDIES/META3.2_DT_allsat_Anticyclonic_long_19930101_20220209.nc'
+# # Loading the eddy netcdf data into a dictionary that corresponds to the netcdf file variable names.
+# # This will not load anything that is defined in "no_load". If you want to load all variables leave
+# # no_load empty (i.e [])
+# eddy,desc = pyEddy_m.AVISO_load(file,no_load)
+# print(desc)
+# # # Here we split the eddy dict down to a set spatial region, and temporal window. This will
+# # # include all eddies even if they appear in the domain for 1 day. We can perform checks later that
+# # # they remain in the domain for a period. If you want all the data, then you don't need to use
+# # # this function.
+# print(len(np.unique(eddy['track'])))
 #
-# fig, ax = plt.subplots()
-# m=EdPlt.base_tracks_start(ax,latb=[-60,10],lonb = [-75,40])
-# EdPlt.plot_eddy_shape(ax,m,eddy,val=0,step=60)
-# EdPlt.base_tracks_end(ax,m)
-# plt.show()
-eddy_t = Edeobs.timeseries_start(eddy,eddy['track'][0])
-eddy_t,desc = Edeobs.add_sstCCI('D:\Data\SST-CCI',eddy_t,desc,radm = 3,plot=0)
-# #eddy_t,desc = Edeobs.add_MODchl('D:\Data\MODIS\DAILY\CHL',eddy_t,desc)
-# eddy_t,desc = Edeobs.add_MODpp('D:\Data\MODIS\DAILY\PP',eddy_t,desc,radm=20,plot=1)
-# #eddy_t,desc = Edeobs.add_wind('D:\Data\CCMP\DAILY',eddy_t,desc,radm=3,plot=0)
-# #eddy_t,desc = Edeobs.add_GlobPAR('D:\Data\GLOBCOLOUR\PAR',eddy_t,desc,radm=3,plot=0)
-# #eddy_t,desc = Edeobs.compNPP_morel(eddy_t,desc,'glob_par_in_median','cci_sst_in_median','modis_chla_in_median','morel_NPP_in')
-Edeobs.timeseries_save(eddy_t,desc,'OUTPUT')
-
+# eddy = pyEddy_m.box_split(eddy,[-60,10],[-70,25],[datetime.datetime(2002,7,1),datetime.datetime(2018,12,31)])
+# print(len(np.unique(eddy['track'])))
+#
+# eddy = pyEddy_m.eddy_length_min(eddy,365)
+# print(len(np.unique(eddy['track'])))
+#
+# #
+# # fig, ax = plt.subplots()
+# # m=EdPlt.base_tracks_start(ax,latb=[-60,10],lonb = [-75,40])
+# # EdPlt.plot_eddy_shape(ax,m,eddy,val=0,step=60)
+# # EdPlt.base_tracks_end(ax,m)
+# # plt.show()
+# eddy_t = Edeobs.timeseries_start(eddy,eddy['track'][0])
+# eddy_t,desc = Edeobs.add_sstCCI('D:\Data\SST-CCI',eddy_t,desc,radm = 3,plot=0)
+# eddy_t,desc = Edeobs.add_cmems('D:\Data\CMEMS\SSS\DAILY',eddy_t,desc,var = 'so',radm = 3,plot=0)
+# eddy_t,desc = Edeobs.add_cmems('D:\Data\CMEMS\MLD\DAILY',eddy_t,desc,var = 'mlotst',radm = 3,plot=0,units='log10(m)',log10=True,depth=False)
+# eddy_t,desc = Edeobs.add_wind('D:\Data\CCMP\V3.1',eddy_t,desc,radm=3)
+# Edeobs.timeseries_save(eddy_t,desc,output_loc)
+# Edeobs.produce_monthly(218589,output_loc,vars=['cmems_mlotst_in_median','cmems_mlotst_out_median','cmems_so_in_median','cmems_so_out_median','cci_sst_in_median','cci_sst_out_median','ccmp_wind_in_median','ccmp_wind_out_median'])
+Edeobs.add_noaa('D:/Data/NOAA_ERSL/DATA/MONTHLY',output_loc,218589)
 #
