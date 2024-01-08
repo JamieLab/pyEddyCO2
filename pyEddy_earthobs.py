24579177c965e28c6cc3e3d6cc423c4ee8795e88
Daniel Ford <77839711+danielford166@users.noreply.github.com>
2024-01-08 08:03:02 +0000

    Implemented flux calcualtions
    This update adds producing the fCO2 using the OceanICU neueral network framework output.
    Fluxengine implementation of the air-sea CO2 flux
    Simple cool and salty skin (i.e fixed values for now.
    Cumulative sea-air CO2 flux calculations
    
    Methodology largely follows Ford et al. (2023) with enhancements and improvements

--- pyEddy_earthobs.py
+++ pyEddy_earthobs.py
@@ -441,3 +441,245 @@ def add_noaa(loc,s_loc,track):
         m = c.createVariable('month_xco2',np.float32,('month_time'))
         m[:] = np.array(xco2)
     c['month_xco2'].units = 'uatm'
+
+def add_era5(loc,s_loc,track,var = 'msl'):
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'r')
+    time = np.array(c['month_time'])
+    lat = np.array(c['month_latitude'])
+    lon = np.array(c['month_longitude'])
+    c.close()
+
+    xco2 = np.zeros((len(time))); xco2[:] = np.nan
+    for i in range(len(time)):
+        timed = pyEddy_m.date_con(int(time[i]))
+        file = glob.glob(os.path.join(loc,timed.strftime("%Y"),timed.strftime("%Y_%m_*.nc")))
+        c = Dataset(file[0],'r')
+        latg = np.abs(np.array(c['latitude']) - lat[i])
+        long = np.abs(np.array(c['longitude']) - lon[i])
+
+        f = np.where(latg == np.min(latg))[0]
+        #print(f)
+        g = np.where(long == np.min(long))[0]
+        #print(g)
+        xco2[i] = np.array(c.variables[var][0,f,g])
+        units = c.variables[var].units
+        long = c.variables[var].long_name
+        c.close()
+    if units == 'Pa':
+        xco2 = xco2/100
+        units = 'mbar'
+
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+    if 'month_'+var in c.variables.keys():
+        c['month_'+var][:] = np.array(xco2)
+    else:
+        m = c.createVariable('month_'+var,np.float32,('month_time'))
+        m[:] = np.array(xco2)
+    c['month_'+var].units = units
+    c['month_'+var].long_name = 'ERA5 ' + long
+
+def add_province(file,s_loc,track,prov_var = False,d2=datetime.datetime(1970,1,15,0,0,0)):
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+    time = np.array(c['month_time'])
+    lat = np.array(c['month_latitude'])
+    lon = np.array(c['month_longitude'])
+    #c.close()
+    prov = np.zeros((len(time))); prov[:] = np.nan
+    d = Dataset(file,'r')
+    latg = np.array(d['latitude'])
+    long = np.array(d['longitude'])
+    timeg = np.array(d['time'])
+    prov_data = np.array(d[prov_var])
+    for i in range(len(timeg)):
+        t = pyEddy_m.date_con(int(timeg[i]), d = d2)
+        #print(t)
+        timeg[i] = pyEddy_m.date_convert(t)
+    d.close()
+    #print(time)
+    print(timeg)
+    for i in range(len(time)):
+
+
+        latv = np.abs(latg - lat[i])
+        lonv = np.abs(long - lon[i])
+        timev = np.abs(timeg - time[i])
+
+        f = np.where(latv == np.min(latv))[0]
+        print(f)
+        g = np.where(lonv == np.min(lonv))[0]
+        w = np.where(timev == np.min(timev))[0]
+        print(g)
+        print(w)
+        print(time[i])
+        prov[i] = prov_data[g,f,w]
+
+    #c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+    if 'month_province' in c.variables.keys():
+        c['month_province'][:] = np.array(prov)
+    else:
+        m = c.createVariable('month_province',np.float32,('month_time'))
+        m[:] = np.array(prov)
+    c['month_province'].units = 'uatm'
+    c['month_province'].file = 'Data from ' + file
+    c['month_province'].long_name = 'fCO2(sw) interpolation province'
+    c.close()
+
+def calc_fco2(net_loc,s_loc,track,province_var = False,input_var = [],add_text='_in',ens=10,oceanicu_frame = 'C:/Users/df391/OneDrive - University of Exeter/Post_Doc_ESA_Contract/OceanICU'):
+    from sklearn.preprocessing import StandardScaler
+    import tensorflow as tf
+    from pickle import dump, load
+    import sys
+
+    sys.path.append(os.path.join(oceanicu_frame))
+    sys.path.append(os.path.join(oceanicu_frame,'Data_Loading'))
+
+    import neural_network_train as nnt
+    """
+    """
+    inps = {}
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'r')
+    for i in input_var:
+        inps[i] = np.array(c[i])
+    prov = np.array(c[province_var])
+    c.close()
+
+    #network_loc = os.path.join(net_loc,'networks')
+
+    uni_prov = np.unique(prov)
+    fco2 = np.zeros((len(prov))); fco2[:] = np.nan
+    fco2_net = np.copy(fco2)
+    fco2_para = np.copy(fco2)
+    fco2_val = np.copy(fco2)
+    val = np.loadtxt(os.path.join(net_loc,'validation','independent_test_rmsd.csv'),delimiter=',')
+
+    for v in uni_prov:
+        f = np.where(prov == v)[0]
+        inp = np.zeros((len(f),len(input_var)))
+        t = 0
+        for j in input_var:
+            inp[:,t] = inps[j][f]
+            t=t+1
+        scalar = load(open(os.path.join(net_loc,'scalars',f'prov_{v}_scalar.pkl'),'rb'))
+        mod_inp = scalar.transform(inp)
+        out_t = np.zeros((len(f),ens)); out_t[:] = np.nan
+        for i in range(ens):
+            mod = tf.keras.models.load_model(os.path.join(net_loc,'networks',f'prov_{v}_model_ens{i}'),compile=False)
+            out_t[:,i] = np.squeeze(mod.predict(mod_inp))
+        fco2[f] = np.nanmean(out_t,axis=1)
+        fco2_net[f] = np.nanstd(out_t,axis=1)*2
+
+        lut = load(open(os.path.join(net_loc,'unc_lut',f'prov_{v}_lut.pkl'),'rb'))
+        lut = lut[0]
+        fco2_para[f] = np.squeeze(nnt.lut_retr(lut,mod_inp))
+        fco2_val[f] = val[int(v),1]
+        outs = {}
+        outs['month_fco2_sw'+add_text] = fco2
+        outs['month_fco2_net_unc'+add_text] = fco2_net
+        outs['month_fco2_para_unc'+add_text] = fco2_para
+        outs['month_fco2_val_unc'+add_text] = fco2_val
+        c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+        for v in list(outs.keys()):
+            if v in c.variables.keys():
+                c[v][:] = np.array(outs[v])
+            else:
+                m = c.createVariable(v,np.float32,('month_time'))
+                m[:] = np.array(outs[v])
+            c[v].units = 'uatm'
+        c.close()
+
+def fluxengine_file_generate(s_loc,track,sub_sst,sss,ws,press,xco2atm,fco2,time = 'month_time',lat='month_latitude',lon='month_longitude',fluxengine_file = 'fluxengine/input.nc'):
+    vars = [sub_sst,sss,ws,press,xco2atm,fco2,time,lat,lon]
+    label = ['t_subskin','salinity','wind_speed','air_pressure','xCO2_atm','fco2sw','time','latitude','longitude']
+    inps = {}
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'r')
+    for i in range(len(vars)):
+        inps[label[i]] = np.array(c[vars[i]])
+    c.close()
+    inps['wind_speed_2'] = inps['wind_speed']**2
+    c = Dataset(fluxengine_file,'w')
+    time_dim = c.createDimension('time', 1)
+    lat_dim = c.createDimension('latitude',len(inps['time']))
+    lon_dim = c.createDimension('longitude',len(inps['time']))
+
+    for v in list(inps.keys()):
+        print(v)
+        if (v == 'latitude') | (v == 'longitude') | (v == 'time'):
+            m = c.createVariable(v,np.float32,(v))
+            if (v == 'time') | (v == 'longitude'):
+                m[:] = np.array(1)
+            else:
+                m[:] = np.array(inps[v])
+        else:
+            m = c.createVariable(v,np.float32,('latitude','longitude','time'))
+            i = inps[v][:]
+            i2 = np.repeat(i[:, np.newaxis], len(inps['time']), axis=1)
+            m[:] = np.array(i2)
+    c.close()
+
+def fluxengine_run(s_loc,track,config_file = 'fluxengine_config_night.conf',start_yr = 1990, end_yr = 2020,var_out = ['OF'],fluxengine_loc = 'fluxengine/flux/1990/01/OceanFluxGHG-month01-jan-1990-v0.nc',
+    add_text = '_in'):
+    """
+    Function to run fluxengine for a eddy.
+    """
+    from fluxengine.core import fe_setup_tools as fluxengine
+    returnCode, fe = fluxengine.run_fluxengine(config_file, start_yr, end_yr, singleRun=True,verbose=True)
+
+    print(returnCode)
+    if returnCode == 0:
+        c = Dataset(fluxengine_loc,'r')
+        d = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+
+        for v in var_out:
+            outs = np.flip(np.array(c[v][0,:,0]))
+            if 'fluxengine_'+v+add_text in d.variables.keys():
+                d['fluxengine_'+v+add_text][:] = np.array(outs)
+            else:
+                m = d.createVariable('fluxengine_'+v+add_text,np.float32,('month_time'))
+                m[:] = np.array(outs)
+            d['fluxengine_'+v+add_text].units = c[v].units
+        c.close()
+        d.close()
+    else:
+        raise RuntimeError('Fluxengine Failed...')
+
+
+def eddy_co2_flux(s_loc,track,fluxn = 'fluxengine_OF',inout = '_in'):
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'r')
+    area = np.array(c['month_effective_area'])
+    time = np.array(c['month_time'])
+    flux = np.array(c[fluxn+inout])
+    c.close()
+    days = []
+    for i in range(len(time)):
+        t = pyEddy_m.date_con(int(time[i]))
+        days.append(calendar.monthrange(int(t.year),int(t.month))[1])
+    print(days)
+    flux = flux * days * area / 1e12
+    cumflux = np.cumsum(flux)
+    print(cumflux)
+    inps = {}
+    inps[fluxn+inout+'_areaday'] = flux
+    inps[fluxn+inout+'_areaday_cumulative'] = cumflux
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+    for v in list(inps.keys()):
+        if v in c.variables.keys():
+            c[v][:] = np.array(inps[v])
+        else:
+            m = c.createVariable(v,np.float32,('month_time'))
+            m[:] = np.array(inps[v])
+        c[v].units = 'Tg C'
+    c.close()
+
+
+def add_variable_zeros(s_loc,track,var_c,var_n,dimension):
+    c = Dataset(os.path.join(s_loc,str(track) +'.nc'),'a')
+    data = np.array(c[var_c])
+    new_data = np.zeros((data.shape))
+    for v in var_n:
+        if v in c.variables.keys():
+            c[v][:] = np.array(new_data)
+        else:
+            m = c.createVariable(v,np.float32,(dimension))
+            m[:] = np.array(new_data)
+        c[v].comment = 'Data was generated with add_variable_zeros, as a fill array.'
+    c.close()
--- pyEddy_ex.py
+++ pyEddy_ex.py
@@ -33,17 +33,32 @@ output_loc = 'OUTPUT'
 # print(len(np.unique(eddy['track'])))
 #
 # #
-# # fig, ax = plt.subplots()
-# # m=EdPlt.base_tracks_start(ax,latb=[-60,10],lonb = [-75,40])
-# # EdPlt.plot_eddy_shape(ax,m,eddy,val=0,step=60)
-# # EdPlt.base_tracks_end(ax,m)
-# # plt.show()
+# # # fig, ax = plt.subplots()
+# # # m=EdPlt.base_tracks_start(ax,latb=[-60,10],lonb = [-75,40])
+# # # EdPlt.plot_eddy_shape(ax,m,eddy,val=0,step=60)
+# # # EdPlt.base_tracks_end(ax,m)
+# # # plt.show()
 # eddy_t = Edeobs.timeseries_start(eddy,eddy['track'][0])
 # eddy_t,desc = Edeobs.add_sstCCI('D:\Data\SST-CCI',eddy_t,desc,radm = 3,plot=0)
 # eddy_t,desc = Edeobs.add_cmems('D:\Data\CMEMS\SSS\DAILY',eddy_t,desc,var = 'so',radm = 3,plot=0)
 # eddy_t,desc = Edeobs.add_cmems('D:\Data\CMEMS\MLD\DAILY',eddy_t,desc,var = 'mlotst',radm = 3,plot=0,units='log10(m)',log10=True,depth=False)
 # eddy_t,desc = Edeobs.add_wind('D:\Data\CCMP\V3.1',eddy_t,desc,radm=3)
 # Edeobs.timeseries_save(eddy_t,desc,output_loc)
-# Edeobs.produce_monthly(218589,output_loc,vars=['cmems_mlotst_in_median','cmems_mlotst_out_median','cmems_so_in_median','cmems_so_out_median','cci_sst_in_median','cci_sst_out_median','ccmp_wind_in_median','ccmp_wind_out_median'])
-Edeobs.add_noaa('D:/Data/NOAA_ERSL/DATA/MONTHLY',output_loc,218589)
-#
+# Edeobs.produce_monthly(218589,output_loc,vars=['cmems_mlotst_in_median','cmems_mlotst_out_median','cmems_so_in_median','cmems_so_out_median','cci_sst_in_median','cci_sst_out_median','ccmp_wind_in_median','ccmp_wind_out_median','effective_area'])
+# Edeobs.add_noaa('D:/Data/NOAA_ERSL/DATA/MONTHLY',output_loc,218589)
+# Edeobs.add_province('D:/ESA_CONTRACT/NN/GCB_TESTING/inputs/neural_network_input.nc',output_loc,218589,prov_var ='prov')
+# Edeobs.add_variable_zeros(output_loc,218589,'month_time',['cci_sst_anom','xco2_atm_anom','cmems_sss_anom','cmems_mld_anom'],'month_time')
+# Edeobs.add_era5('D:/Data/ERA5/MONTHLY/DATA',output_loc,218589,var='msl')
+# Edeobs.calc_fco2('D:/ESA_CONTRACT/NN/GCB_TESTING',output_loc,218589,province_var = 'month_province',
+#     input_var=['month_cci_sst_in_median','month_xco2','month_cmems_so_in_median','month_cmems_mlotst_in_median','cci_sst_anom','xco2_atm_anom','cmems_sss_anom','cmems_mld_anom'])
+# Edeobs.calc_fco2('D:/ESA_CONTRACT/NN/GCB_TESTING',output_loc,218589,province_var = 'month_province',
+#     input_var=['month_cci_sst_out_median','month_xco2','month_cmems_so_out_median','month_cmems_mlotst_out_median','cci_sst_anom','xco2_atm_anom','cmems_sss_anom','cmems_mld_anom'],
+#     add_text='_out')
+# Edeobs.fluxengine_file_generate(output_loc,218589,sub_sst = 'month_cci_sst_in_median',sss = 'month_cmems_so_in_median', ws = 'month_ccmp_wind_in_median',press = 'month_msl',
+#     xco2atm = 'month_xco2',fco2 = 'month_fco2_sw_in')
+# Edeobs.fluxengine_run(output_loc,218589)
+Edeobs.fluxengine_file_generate(output_loc,218589,sub_sst = 'month_cci_sst_out_median',sss = 'month_cmems_so_out_median', ws = 'month_ccmp_wind_out_median',press = 'month_msl',
+    xco2atm = 'month_xco2',fco2 = 'month_fco2_sw_out')
+Edeobs.fluxengine_run(output_loc,218589,add_text='_out')
+Edeobs.eddy_co2_flux(output_loc,218589)
+Edeobs.eddy_co2_flux(output_loc,218589,inout='_out')
